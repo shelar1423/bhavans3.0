ce60c76b134d975dcd49c9e8e8c41d91507a44b2
D Abhinav Vardhan <vardhanabhi22@gmail.com>
2022-12-12 01:29:22 +0530

    Finally done!!

--- app/src/main/java/com/example/onboardingcompose/screen/WelcomeScreen.kt
+++ app/src/main/java/com/example/onboardingcompose/screen/WelcomeScreen.kt
@@ -4,10 +4,11 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,10 +21,9 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.onboardingcompose.R
-import com.example.onboardingcompose.navigation.NavigationItem
+import com.example.onboardingcompose.navigation.OnBoardingPage
 import com.example.onboardingcompose.navigation.Screen
 import com.example.onboardingcompose.ui.theme.ButtonBlue
-import com.example.onboardingcompose.navigation.OnBoardingPage
 import com.example.onboardingcompose.viewmodel.WelcomeViewModel
 import com.google.accompanist.pager.*
 
@@ -129,7 +129,7 @@ fun FinishButton(
             Button(
                 onClick = onClick,
                 colors = ButtonDefaults.buttonColors(
-                    contentColor = Color.White
+                    contentColor = Color.Blue
                 )
             ) {
                 Text(text = "Finish", color = Color.White)
